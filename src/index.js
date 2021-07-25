81ccd697c3956575adf62dac3fbe95b861c3b1d1
アドビ 波江野 <tos25560@adobe.com>
2021-07-25 11:46:07 +0900

    learn implement store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
